81a886d94631056823e37542d1dbe88e6f36ea9f
bxxstian <seb_faull@hotmail.co.uk>
2019-06-10 14:16:16 +0100

    Add test for body

--- src/test/java/com/spartaglobal/restAssuredIntro/getRequest/GetData.java
+++ src/test/java/com/spartaglobal/restAssuredIntro/getRequest/GetData.java
@@ -1,7 +1,7 @@
 package com.spartaglobal.restAssuredIntro.getRequest;
 
 import com.spartaglobal.restAssuredIntro.BaseClass;
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,10 +10,18 @@ public class GetData extends BaseClass {
 
     @Test
     public void testResponseCode() {
-        Response resp = RestAssured.get(BASE_ENDPOINT);
+        Response resp = get(BASE_ENDPOINT);
         int code = resp.getStatusCode();
 
         Assert.assertEquals(code, 200);
+    }
+
+    @Test
+    public void testBody() {
+        Response resp = get(BASE_ENDPOINT);
+        String data = resp.asString();
+
+        System.out.println("Data is " + data);
     }
 
 }
